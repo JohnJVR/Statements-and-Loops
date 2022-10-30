391902caaaf9a4b5ada4b0dc1504a4ddbdb06240
JohnJVR <58113684+JohnJVR@users.noreply.github.com>
2022-10-30 22:31:32 +0530

    Add files via upload

---
+++ 1 - for loops basic.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 17:05:35 2022
+
+@author: Acer
+"""
+
+### Loops Basic
+
+for i in range(5): 
+    print("JoHn")
+    
+###################################################################
+
+for i in range(0,15):
+    print(i)
+
+####################################################################
+
+for i in range (0,15,2):
+    print(i)
+
+for i in range (0,50,3):
+    print(i)
+    
+#####################################################################
+
+a=int(input("enter a number"))
+for i in range(a):
+    print(i,end="")
